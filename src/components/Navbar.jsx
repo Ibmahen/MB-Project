9e12e76fb0685a446f93cc074416011930703077
Ibmahen <ibmahendra4@gmail.com>
2025-05-27 16:35:06 +0700

    update nav-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,20 @@
-function Navbar() {
+export default function Navbar() {
   return (
-    // <div className="bg-white shadow">
-
-
-    // </div>
-
-
-
-
-    <div className="nav">
-      <button className="nav-btn">Home</button>
-      <button className="nav-btn">About</button>
-      <button className="nav-btn">Our Team</button>
-      <button className="nav-btn">Services</button>
-      <button className="nav-btn">Galery</button>
-      <button className="nav-btn">Get Contact</button>
+    <div className="flex justify-between items-center mt-5 text-black sticky top-0 mx-10">
+      <div className="nav-side">
+        <a href="/">Logo</a>
+      </div>
+      <div className="nav-center">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/our-team">Our Team</a>
+        <a href="/services">Services</a>
+        <a href="/gallery">Gallery</a>
+        <a href="/contact" className="bg-[#2C89F3] text-white w-[157px] h-[50px] center-kabeh rounded-[8px]">Get Contact</a>
+      </div>
+      <div className="nav-side">
+        <a href="/sign-in">Sign in</a>
+      </div>
     </div>
   );
 }
-
-export default Navbar;
